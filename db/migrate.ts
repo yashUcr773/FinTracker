5c78c0355f1b19f122740f0d75e3b2f59d642680
yashUcr773 <yagga004@ucr.edu>
2024-05-12 08:35:01 +0530

    initialize drizzle

---
+++ db/migrate.ts
@@ -0,0 +1,21 @@
+import { neon } from '@neondatabase/serverless'
+import { config } from 'dotenv'
+import { drizzle } from 'drizzle-orm/neon-http'
+import { migrate } from 'drizzle-orm/neon-http/migrator'
+
+config({ path: ".env" })
+
+const sql = neon(process.env.DRIZZLE_DATABASE_URL!)
+const db = drizzle(sql)
+
+const main = async () => {
+    try {
+        await migrate(db, { migrationsFolder: "drizzle" })
+
+    } catch (e) {
+        console.error('Error during migration', e)
+        process.exit(1)
+    }
+}
+
+main();
