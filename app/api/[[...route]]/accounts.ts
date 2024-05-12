faaf63dabd2634c0570406d0ea075700b0c2334d
yashUcr773 <yagga004@ucr.edu>
2024-05-12 09:27:50 +0530

    minor fix

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { db } from "@/db/drizzle"
 import { accounts } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { HTTPException } from 'hono/http-exception'
+import { eq } from "drizzle-orm";
 
 const app = new Hono()
     .get("/",
@@ -12,12 +12,10 @@ const app = new Hono()
             const auth = getAuth(c)
 
             if (!auth?.userId) {
-                throw new HTTPException(401, {
-                    res: c.json({ error: 'unauthorized' }, 401)
-                })
+                return c.json({ error: 'unauthorized' }, 401)
             }
 
-            const data = await db.select({ id: accounts.id, name: accounts.name }).from(accounts)
+            const data = await db.select({ id: accounts.id, name: accounts.name }).from(accounts).where(eq(accounts.userId, auth.userId))
             return c.json({ data })
         })
 
